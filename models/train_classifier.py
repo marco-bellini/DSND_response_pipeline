a67abec45f9e1cb43db6d2f56d51ab096860e7ba
marco-bellini <marco.bellini.it@gmail.com>
2019-04-05 11:21:25 +0200

    type: feat
    added model fitting

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,13 +1,68 @@
 import sys
+import pandas as pd
+import numpy as np
 
+from sqlalchemy import create_engine
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import nltk
+import re
+from nltk.stem.wordnet import WordNetLemmatizer
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import fbeta_score, make_scorer
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
+from sklearn.multioutput import MultiOutputClassifier
+
+from sklearn.model_selection import train_test_split
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier,AdaBoostClassifier
+
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import make_scorer, accuracy_score, f1_score, fbeta_score, classification_report
+
+import sklearn.metrics as met
+from sklearn.utils.fixes import signature
+import pickle
 
 def load_data(database_filepath):
-    pass
+    """
+
+    :param database_filepath:
+    :return:
+    """
+
+    # database_filepath='sqlite:///data//InsertDatabaseName.db'
+    engine = create_engine(database_filepath)
+    df = pd.read_sql_table('InsertTableName', engine)
+    X = df['message']
+    Y = df.iloc[:, 4:]
+    category_names=list(df.columns)
+
+    return(X,Y,category_names)
 
 
 def tokenize(text):
-    pass
+    """
+    tokenizes the messagages into words (lowercase, remove punctuation, split, remove stopwords and lemmatize with WordNet)
+    Inputs:
+    - text: string
 
+    """
+
+    text = text.lower()
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text)
+    words = text.split()
+    words = [w for w in words if w not in stopwords.words("english")]
+    lemmed = [WordNetLemmatizer().lemmatize(w, pos='v') for w in words]
+
+    return (lemmed)
 
 def build_model():
     pass
@@ -21,7 +76,74 @@ def save_model(model, model_filepath):
     pass
 
 
-def main():
+def mo_confusion_matrix(y_true, y_pred):
+    """
+
+    :param y_true:
+    :param y_pred:
+    :return:
+
+    """
+    cm = np.zeros((2, 2, y_true.shape[1])).astype(int)
+
+    c = 0
+    for column in y_true.columns:
+        cm[:, :, c] = met.confusion_matrix(y_test[column], y_pred[:, c])
+        c += 1
+    tn = cm[0, 0, :].ravel()
+    fp = cm[0, 1, :].ravel()
+    fn = cm[1, 0, :].ravel()
+    tp = cm[1, 1, :].ravel()
+
+    return (tn, fp, fn, tp)
+
+def mo_weighted_scorer(y_true, y_pred, weights={'tp': 1, 'tn': 1, 'fn': 1, 'fp': 1}, class_weights=None,
+                       adjust_for_frequency=False,
+                       return_sum=True):
+    """
+
+    :param y_true:
+    :param y_pred:
+    :param weights:
+    :param class_weights:
+    :param adjust_for_frequency:
+    :param return_sum:
+    :return:
+    """
+    # score with custom weights for errors
+
+    tn, fp, fn, tp = mo_confusion_matrix(y_test, y_pred)
+    score = tn * weights['tn'] + tp * weights['tp'] - fn * weights['fn'] - fp * weights['tp']
+
+    if adjust_for_frequency:
+        # adjusting the weights by the frequency
+
+        real_disasters = tp + fn
+        class_inv_frequencies = 1.0 / real_disasters
+        class_inv_frequencies[real_disasters == 0] = 0
+        # normalize the inv. frequencies
+        class_inv_frequencies /= class_inv_frequencies.max()
+        #         print()
+        #         print(class_inv_frequencies)
+        #         print()
+
+        if not class_weights is None:
+            class_weights *= class_inv_frequencies
+        else:
+            # the score is just adjusted by the inverse of frequency
+            class_weights = class_inv_frequencies
+
+    if not class_weights is None:
+        # adjust score by weights
+        score *= class_weights
+
+    if return_sum:
+        return np.sum(score)
+    else:
+        return score
+
+
+def udacity_main():
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
@@ -48,6 +170,61 @@ def main():
               'save the model to as the second argument. \n\nExample: python '\
               'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
 
+def create_pipelines():
+    """
+
+    :return:
+    """
+
+    pipe_DecisionTree = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(DecisionTreeClassifier())),
+    ])
+    par_DecisionTree = {
+        'clf__estimator__min_samples_split': [0.1, .9],
+    }
+
+    pipe_RandomForest = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier())),
+    ])
+    par_RandomForest = {
+        'vect__ngram_range': ((1, 1), (1, 2)),
+        'vect__max_df': (0.75, 1.0),
+        'clf__estimator__n_estimators': [20,50, 100],
+    }
+
+
+    pipelines={'RF':[pipe_RandomForest,par_RandomForest] , 'DT':[pipe_DecisionTree,par_DecisionTree]}
+    return(pipelines)
+
+
+def main():
+
+    database_filepath = 'sqlite:///data//InsertDatabaseName.db'
+    X, Y, category_names = load_data(database_filepath)
+    print('X:', X.shape)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
+
+    classifier='DT'
+    print(classifier)
+
+    pipelines=create_pipelines()
+    model = pipelines[classifier][0]
+    parameters = pipelines[classifier][1]
+
+    cv = GridSearchCV(model, param_grid=parameters, verbose=2, cv=3)
+
+    filename='^%2d_%s' % (1,classifier)
+    pickle.dump(cv.best_estimator_, open(filename + '_model.pkl', 'wb'))
+    pickle.dump(cv.best_params_, open(filename + '_params.pkl', 'wb'))
+    pickle.dump(y_train, open(filename + '_ytrain.pkl', 'wb'))
+
+    y_pred = cv.best_estimator_.predict(X_test)
+    pickle.dump(y_pred, open(filename + '_ypred.pkl', 'wb'))
 
 if __name__ == '__main__':
     main()
